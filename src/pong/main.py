a68e465d7fdb421b786fe71a772dcf7e836c4c69
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2020-10-12 22:27:01 -0300

    feat: add collision validation

--- src/pong/main.py
+++ src/pong/main.py
@@ -7,6 +7,8 @@ import pygame
 HEIGHT = 600
 WIDTH = 800
 
+X_START = 0
+
 # Game Speed
 FPS = 144
 
@@ -69,6 +71,26 @@ class Rect(BaseComponent):
     def y(self, value):
         self.rect.y = value
 
+    @property
+    def left(self):
+        return self.rect.left
+
+    @property
+    def right(self):
+        return self.rect.right
+
+    @property
+    def top(self):
+        return self.rect.top
+
+    @property
+    def bottom(self):
+        return self.rect.bottom
+
+    @property
+    def center_y(self):
+        return self.rect.centery
+
     def draw(self, color):
         pygame.draw.rect(screen, color, self.rect)
 
@@ -86,9 +108,17 @@ class Ball(Rect):
         center_x, center_y = x - BALL_SIZE // 2, y - BALL_SIZE // 2
         return center_x, center_y
 
+    def change_y_direction(self):
+        d_x, d_y = self.direction
+        self.direction = (d_x, d_y * -1)
+
+    def change_x_direction(self):
+        d_x, d_y = self.direction
+        self.direction = (d_x * -1, d_y)
+
     def move(self):
         d_choices = [-1, 1]
-        if self.direction == (None, None):
+        if set(self.direction) == {None}:
             self.direction = (random.choice(d_choices), random.choice(d_choices))
         else:
             d_x, d_y = self.direction
@@ -119,9 +149,13 @@ class Field(BaseComponent):
 class Player(Rect):
     def __init__(self, right_side=False):
         self.right_side = right_side
+        self.score = 0
 
         x, y = self._get_start_position()
         super().__init__(x, y, PLAYER_WIDTH, PLAYER_HEIGHT)
+
+    def score_point(self):
+        self.score += 1
 
     def _get_player_side(self):
         if self.right_side:
@@ -149,9 +183,51 @@ class Game(BaseComponent):
         self.player_1 = Player()
         self.player_2 = Player(right_side=True)
 
+    def reset(self):
+        pass
+
+    def score_a_point(self, player):
+        player.score += 1
+        self.reset()
+
+    def hit_player(self, player):
+        return (
+            player.right == self.ball.left
+            and player.bottom <= self.ball.center_y <= player.top
+        )
+
+    def hit_border(self, x_border):
+        return self.ball.left == x_border or self.ball.right == x_border
+
+    def _check_player_collisions(self):
+        if self.hit_player(self.player_1):
+            self.ball.change_x_direction()
+        elif self.hit_player(self.player_2):
+            self.ball.change_x_direction()
+
+    def _check_field_collisions(self):
+        if self.ball.top == 0:
+            self.ball.change_y_direction()
+        elif self.ball.bottom == HEIGHT:
+            self.ball.change_y_direction()
+
+        if self.hit_border(X_START):
+            self.score_a_point(self.player_2)
+        elif self.hit_border(WIDTH):
+            self.score_a_point(self.player_1)
+
+    def _check_collisions(self):
+        self._check_field_collisions()
+        self._check_player_collisions()
+
     def draw(self):
+        # Check collisions before making any movement
+        self._check_collisions()
+
+        # Moves the ball
         self.ball.move()
 
+        # Draw items on the the screen
         self.field.draw()
         self.player_1.draw()
         self.player_2.draw()
